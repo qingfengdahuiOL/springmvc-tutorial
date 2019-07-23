13bb762b08a780a834de8d064085ec6595aac4b4
qingfengdahuiOL <52949063+qingfengdahuiOL@users.noreply.github.com>
2019-07-23 16:04:00 +0800

    上传文件

---
+++ hello_world.py
@@ -0,0 +1,43 @@
+
+print("hello python world")
+print ("nihao")
+message = "hello world"
+print(message)
+
+message = "hello python crash course world"
+print (message)
+
+student_name="hahaha2"
+print (student_name)
+
+ha1='I told my friend,"python is my favorite language"'
+print(ha1.upper())
+ha="the language 'python' is named after month python, not the snake"
+print(ha.title())
+
+print(ha1.lower())
+
+first_name='liu'
+last_name='zongwei'
+name=first_name+' '+last_name
+print('hello,'+name.title()+"!")
+
+print('python')
+print('\tpython')
+print('languagr:\npython\nC\njava')
+print('languagr:\n python\n C\n java')
+print('languagr:\n\tpython\n\tC\n\tjava')
+
+favorite_language='python '
+
+a="' python '"
+print(a)
+print(a.rstrip())
+print(a.lstrip())  # 问好
+print(a.strip())
+print('中文')  #改变文字类型就可以显示中文
+
+print(3/2)
+print(2*4)
+print(3+5)
+
